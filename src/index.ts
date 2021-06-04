23bc3929ddb88c1ad42a1284c31376b4a772022e
xavieresrb <xavieres@gmail.com>
2021-06-04 15:43:44 +0200

    fix: refactor with inheritance

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'newer name', age: 3 });
+const user = User.buildUser({ id: 1 });
 
-user.on('save', () => {
+user.on('change', () => {
   console.log(user);
 });
 
-user.save();
+user.fetch();
