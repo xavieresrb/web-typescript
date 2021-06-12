f7f735651bae3d0d3005570fdfbefbe3145c0fad
xavieresrb <xavieres@gmail.com>
2021-06-12 13:42:09 +0200

    fix: link model to the view

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
+import { UserForm } from './views/UserForm';
 import { User } from './models/User';
 
-const collection = User.buildUserCollection();
+const user = User.buildUser({ name: 'Peter', age: 25 });
 
-collection.on('change', () => {
-  console.log(collection);
-});
+const userForm = new UserForm(document.getElementById('root'), user);
 
-collection.fetch();
+userForm.render();
---
+++ src/views/UserForm.ts
@@ -0,0 +1,54 @@
+import { User } from '../models/User';
+
+export class UserForm {
+  constructor(public parent: Element, public model: User) {}
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onHeaderHover,
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('button clicked');
+  }
+
+  onHeaderHover(): void {
+    console.log('h1 was hovered over');
+  }
+
+  template(): string {
+    return `
+      <div>
+        <h1>UserForm</h1>
+        <div>User name:${this.model.get('name')}</div>
+        <div>User age:${this.model.get('age')}</div>
+        <input/>
+        <button/>
+      </div>    
+    `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
+  render(): void {
+    const templateElement = document.createElement('template');
+
+    templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
+
+    this.parent.append(templateElement.content);
+  }
+}
